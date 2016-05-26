ccd6f921fc1b2f7a8e36b169d047e176406a017f
Guillaume853 <autierguillaume1@gmai.com>
2016-05-26 07:48:44 +0100

    add resources routing for products and CRUD actions for products

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,23 @@ Rails.application.routes.draw do
 
   get "/team" => "pages#team"
   get "/contact" => "pages#join_us"
-  get "/products" => "products#index"
-  get "/products/:id" => "products#show"
-  #la méthode (à droite) et le nom de page (à gauche) n'ont pas forcément le même nom
 
+  resources :products #, only: [:new, :create, :index, :show]
+
+  # #Read action routes
+  # get "/products" => "products#index"
+  # get "/products/:id" => "products#show"
+  # #la méthode (à droite) et le nom de page (à gauche) n'ont pas forcément le même nom
+
+  # #Create action routes
+  # get "/products/new" => "products#new"
+  # post "/products" => "products#create"
+
+  # #Update action routes
+  # get "/products/:id/edit" => "products#edit"
+  # patch "/products/:id" => "products#update"
+
+  # #Delete action routes
+  # delete "/products/:id" => "products#destroy"
 
 end
